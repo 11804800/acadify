430072b27c8eb57fd4c621d44d9fd5cd4be9575d
Nikhil Pathak <nikhilpathak2602@gmail.com>
2025-11-09 14:31:58 +0530

    Router Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import HeaderComponent from './Components/HeaderComponent'
 import FooterComponent from './Components/FooterComponent'
+import { Route, Routes } from 'react-router'
+import StudentDashboard from './Pages/StudentDashboard'
 
 function App() {
   return (
-    <div>
+    <>
       <HeaderComponent />
+      <Routes>
+        <Route path='/' element={<StudentDashboard />} />
+      </Routes>
       <FooterComponent />
-    </div>
+    </>
   )
 }
 
